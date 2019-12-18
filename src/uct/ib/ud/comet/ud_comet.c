658b09cbce4ae20f87b89a44a1572a139e53ef4c
Alex Margolin <alex.margolin@huawei.com>
2019-12-18 11:28:47 +0200

    COMET: working on send/recv

--- src/uct/ib/ud/comet/ud_comet.c
+++ src/uct/ib/ud/comet/ud_comet.c
@@ -4,9 +4,8 @@
 * See file LICENSE for terms.
 */
 #include "ud_comet.h"
-#include "../accel/ud_mlx5.h"
 
-#include <comet_lib.h>
+#include <uct/sm/mm/base/mm_md.h>
 
 static ucs_config_field_t uct_ud_comet_iface_config_table[] = {
   {"UD_", "", NULL,
@@ -48,24 +47,32 @@ extern UCS_CLASS_INIT_FUNC(uct_ud_mlx5_iface_t,
 static unsigned uct_ud_comet_iface_progress(uct_iface_h tl_iface)
 {
     uct_ud_comet_iface_t *iface = ucs_derived_of(tl_iface, uct_ud_comet_iface_t);
-    ucs_status_t status;
-    unsigned idx, count;
 
-    for (idx = iface->last_table_comp_idx + 1;
-         idx < iface->last_table_comp_idx;
-         idx = (idx + 1) % iface->table_cnt) {
-        if (iface->tables[idx].) {
-            void *base_address = ???;
-            uct_am_handler();
+    unsigned idx;
+    uint64_t prefix;
+    for (idx = 0; idx < iface->table_cnt; idx++) {
+        if ((prefix = iface->tables[idx].incoming_prefix) != 0) {
+            void *data = iface->tables[idx].incoming_data_va;
+            size_t len = iface->tables[idx].data_length;
 
-            self->next_prefix = idx;
-            uct_iface_trace_am(&iface->super.super, UCT_AM_TRACE_TYPE_RECV,
-                    am_id, base_address, length, "RX: AM_ZCOPY");
+            iface->tables[idx].tag_ctx.completed_cb(data /*tag_ctx*/,
+                    0 /*stag*/, prefix /*imm*/, len, UCS_OK);
+
+            iface->tables[idx].incoming_prefix = 0;
             return 1;
         }
     }
 
-    return uct_ud_mlx5_iface_progress();
+    return iface->super_progress(tl_iface); /* uct_ud_mlx5_iface_progress */
+}
+
+ucs_status_t uct_ud_comet_ep_send()
+{
+    /*
+      comet_packet_header_init(struct comet_packet_header *packet_header,
+            uint8_t table_id, uint8_t slot_id, uint64_t prefix);
+     */
+    return UCS_OK;
 }
 
 ucs_status_t uct_ud_comet_iface_tag_recv_zcopy(uct_iface_h iface, uct_tag_t tag,
@@ -74,28 +81,28 @@ ucs_status_t uct_ud_comet_iface_tag_recv_zcopy(uct_iface_h iface, uct_tag_t tag,
                                                size_t iovcnt,
                                                uct_tag_context_t *ctx)
 {
-    uct_ud_comet_iface_t *iface = ucs_derived_of(tl_iface, uct_ud_comet_iface_t);
+    uct_ud_comet_iface_t *comet = ucs_derived_of(iface, uct_ud_comet_iface_t);
     uint64_t comet_prefix       = (uint64_t)tag;
-    void *base_address          = (void*)iov; /* WARNING: parameter type abuse*/
+    void *base_address          = iov[0].buffer;
     uint32_t table_index        = iovcnt;     /* WARNING: parameter type abuse*/
-    uct_ud_comet_table_t *table = iface->tables[table_index];
+    uct_ud_comet_table_t *table = &comet->tables[table_index];
 
     /* Sanity checks */
     ucs_assert(comet_prefix != 0);
     ucs_assert(tag_mask == (uct_tag_t)-1);
     ucs_assert(ctx->completed_cb != NULL);
 
+    table->tag_ctx = *ctx;
+    table->data_length = iov[0].length;
     if (table->incoming_data_va != base_address) {
         table->incoming_data_va = base_address;
         table->incoming_data_pa = comet_buffer_phys_address_get(base_address);
     }
 
-    comet_packet_header_init(struct comet_packet_header *packet_header,
-            uint8_t table_id, uint8_t slot_id, uint64_t prefix);
-
     /* Set the request in the COMET table */
-    int ret = comet_recv(comet_iface->comet_ref, table_index,
-                         &table->incoming_prefix, base_address);
+    int ret = comet_recv(comet->comet_ref, table_index,
+                         (uintptr_t)&table->incoming_prefix,
+                         (uintptr_t)base_address);
     return ret ? UCS_ERR_IO_ERROR : UCS_OK;
 }
 
@@ -116,10 +123,11 @@ static UCS_CLASS_INIT_FUNC(uct_ud_comet_iface_t,
             ucs_derived_of(tl_config, uct_ud_comet_iface_config_t);
     UCS_CLASS_CALL_SUPER_INIT(uct_ud_mlx5_iface_t, md,
                               worker, params, &config->super.super.super);
+    self->super_progress = self->super.super.super.super.super.ops.iface_progress;
     ud_comet_ops_mlx5_reuse_initialize(&uct_ud_comet_iface_ops);
 
     /* Get the attributes of this MD for connection establishment later */
-    status = uct_mm_md_query(md, &self->md_attr);
+    ucs_status_t status = uct_mm_md_query(md, &self->md_attr);
     if (status != UCS_OK) {
         return status;
     }
@@ -137,6 +145,7 @@ static UCS_CLASS_INIT_FUNC(uct_ud_comet_iface_t,
 
     /* Query the COMET device capabilities */
     unsigned num_comet_devices;
+    const struct comet_capabilities *comet_devices;
     ret = comet_query_capabilities(&comet_devices, &num_comet_devices);
     if (ret != 0) {
         ucs_error("Failed to query COMET capabilities");
@@ -148,10 +157,9 @@ static UCS_CLASS_INIT_FUNC(uct_ud_comet_iface_t,
                                    "COMET tables array");
     unsigned idx;
     for (idx = 0; idx < num_comet_devices; idx++) {
-        self->tables[idx].collective_type = UCT_UD_COMET_COLL_TYPE_ALLREDUCE; // TODO: detect
-        self->tables[idx].incoming_prefix = 0;
+        self->tables[idx].collective_type  = UCT_UD_COMET_COLL_TYPE_REDUCE; // TODO: detect
         self->tables[idx].incoming_data_va = NULL;
-        self->tables[idx].incoming_data_pa = NULL;
+        self->tables[idx].incoming_prefix  = 0;
     }
 
     return UCS_OK;
--- src/uct/ib/ud/comet/ud_comet.h
+++ src/uct/ib/ud/comet/ud_comet.h
@@ -6,12 +6,10 @@
 #ifndef _UD_COMET_H_
 #define _UD_COMET_H_
 
-#include <uct/ib/ud/accel/ud_mlx5_common.h>
+#include <comet_lib.h>
 
-#include <uct/ib/ud/base/ud_iface.h>
 #include <uct/ib/ud/base/ud_ep.h>
-
-#include "../accel/ud_mlx5.h"
+#include <uct/ib/ud/accel/ud_mlx5.h>
 
 typedef struct {
     uct_ud_iface_config_t               super;
@@ -30,36 +28,38 @@ typedef struct {
 #define UCT_UD_COMET_TL_NAME "ud_comet"
 
 enum uct_ud_comet_coll_type {
-    UCT_UD_COMET_COLL_TYPE_ALLREDUCE = COMET_TABLE_OPERATION_REDUCE,
+    UCT_UD_COMET_COLL_TYPE_REDUCE    = COMET_TABLE_OPERATION_REDUCE,
     UCT_UD_COMET_COLL_TYPE_ALLTOALL  = COMET_TABLE_OPERATION_ALLTOALL
 };
 
 typedef struct uct_ud_comet_table {
-    enum uct_ud_comet_coll_type collective_type;
-    uct_tag_t                   incoming_prefix;
-    void*                       incoming_data_va;
-    void*                       incoming_data_pa;
+    volatile uint64_t           incoming_prefix;  /* Prefix is placed here upon completion */
+    enum uct_ud_comet_coll_type collective_type;  /* Type of collective set for this table */
+    void*                       incoming_data_va; /* Virtual address to write the payload */
+    void*                       incoming_data_pa; /* Physical address to write the payload */
+    size_t                      data_length;      /* Send/recv buffer (data) length */
+    uct_tag_context_t           tag_ctx;          /* Tag context - for completion callbacks */
 } uct_ud_comet_table_t;
 
 typedef struct uct_tl_comet_device_resource {
-    char                     tl_name[UCT_TL_NAME_MAX];   /**< Transport name */
-    char                     dev_name[UCT_COMET_DEVICE_NAME_MAX]; /**< Hardware device name */
-    const struct comet_capabilities *comet_capabilities_p; /* Pointer to COMET capabilities */
+    uct_device_type_t type;                                /**< Device type */
+    char              tl_name[UCT_TL_NAME_MAX];            /**< Transport name */
+    char              dev_name[UCT_COMET_DEVICE_NAME_MAX]; /**< Hardware device name */
 
-    uct_device_type_t         type;     /**< Device type. To which UCT group it belongs to */
+    const struct comet_capabilities *comet_capabilities_p; /* Pointer to COMET capabilities */
 } uct_tl_comet_device_resource_t;
 
 typedef uct_ud_mlx5_ep_t uct_comet_ep_t;
 
 typedef struct uct_ud_comet_iface {
     uct_ud_mlx5_iface_t   super;
+    unsigned            (*super_progress)(uct_iface_h tl_iface);
 
     uint32_t              sm_proc_cnt;         /* provided PPN information */
     uct_md_attr_t         md_attr;             /* memory domain attributes */
     void                 *comet_ref;           /* device reference / handle */
     uint32_t              comet_device_index;  /* device index */
     uint32_t              table_cnt;           /* number of tables */
-    uint32_t              last_table_comp_idx; /* index of the last completed table */
     uct_ud_comet_table_t *tables;              /* per-table information */
 } uct_ud_comet_iface_t;
 
