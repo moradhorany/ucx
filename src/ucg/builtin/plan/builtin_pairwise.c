5f2e4c56847bd7501e855133f4db51613faab845
Alex Margolin <alex.margolin@huawei.com>
2019-12-22 08:50:41 +0200

    BUILTIN: Cosmetic changes for the pairwise toplogy

--- src/ucg/builtin/plan/builtin_pairwise.c
+++ src/ucg/builtin/plan/builtin_pairwise.c
@@ -18,25 +18,22 @@ ucs_status_t ucg_builtin_pairwise_create(ucg_builtin_group_ctx_t *ctx,
         const ucg_collective_type_t *coll_type,
         ucg_builtin_plan_t **plan_p)
 {
-    ucs_status_t status = UCS_OK;
-
     /* Calculate the number of pairwise steps */
-    unsigned proc_count = group_params->member_count;
-    ucg_step_idx_t nSteps = proc_count;
+    ucg_group_member_index_t proc_idx, proc_count = group_params->member_count;
 
     /* Allocate memory resources */
-    size_t alloc_size =          sizeof(ucg_builtin_plan_t) +
-                       			(sizeof(ucg_builtin_plan_phase_t)
-                   			  + (nSteps * sizeof(uct_ep_h)));/* every phase has no more than two endpoints! */
+    size_t alloc_size = sizeof(ucg_builtin_plan_t) +
+                        sizeof(ucg_builtin_plan_phase_t) +
+                        ((proc_count - 1) * sizeof(uct_ep_h));
 
     ucg_builtin_plan_t *pairwise    = (ucg_builtin_plan_t*)UCS_ALLOC_CHECK(alloc_size, "pairwise topology");
     ucg_builtin_plan_phase_t *phase = &pairwise->phss[0];
-    uct_ep_h *next_ep               = (uct_ep_h*)(phase + nSteps);
-    pairwise->ep_cnt                = nSteps;
+    pairwise->ep_cnt                = proc_count;
     pairwise->phs_cnt               = 1;
 
+    ucs_assert((ucg_group_member_index_t)((typeof(pairwise->ep_cnt))-1) > proc_count);
+
     /* Find my own index (rank) */
-    /* Alex: use group_params' member to get my_index */
     ucg_group_member_index_t my_index = 0;
     while ((my_index < proc_count) && (group_params->distance[my_index] != UCG_GROUP_MEMBER_DISTANCE_SELF)) {
         my_index++;
@@ -51,22 +48,18 @@ ucs_status_t ucg_builtin_pairwise_create(ucg_builtin_group_ctx_t *ctx,
     phase->method = UCG_PLAN_METHOD_PAIRWISE;
     phase->step_index = step_idx;
 
-    for(int step_idx = 0; step_idx < (int) nSteps-1; step_idx++){
-    	phase->step_index = step_idx;
-
-    	ucg_group_member_index_t peer_index_dst = (my_index + (step_idx+1) ) % proc_count;
-
+    for(proc_idx = 1; proc_idx < proc_count; proc_idx++) {
         /* Connect to receiver for second EP */
-        status = ucg_builtin_connect( ctx, peer_index_src, phase, phase_ep_index, 0 );
-        if( status != UCS_OK ){
-        	return status;
+        ucg_group_member_index_t next_peer = (my_index + proc_idx) % proc_count;
+        ucs_status_t status = ucg_builtin_connect(ctx, next_peer, phase, proc_idx - 1, 0);
+        if (status != UCS_OK) {
+            return status;
         }
     }
 
     pairwise->super.my_index = my_index;
     *plan_p = pairwise;
-
-	return status;
+    return UCS_OK;
 }
 
 
